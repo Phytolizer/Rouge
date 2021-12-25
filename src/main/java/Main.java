1c3934738e34bea698eaf8b8e84ec22d11688d02
Kyle Coffey <mariorules24@gmail.com>
2021-12-25 08:27:13 -0600

    basic game usage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import de.gurkenlabs.litiengine.Game;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Game.init(args);
+        Game.start();
     }
 }
