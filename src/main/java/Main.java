c914bad82e55e657db23f88c8a2782826b781b73
Kyle Coffey <mariorules24@gmail.com>
2021-12-25 08:48:32 -0600

    lwjgl?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
-import de.gurkenlabs.litiengine.Game;
 
 public class Main {
     public static void main(String[] args) {
-        Game.init(args);
-        Game.start();
+        System.out.println("Hello, World!");
     }
 }
